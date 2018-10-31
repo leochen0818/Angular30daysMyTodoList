a34319b155d5191989061e5f10a02ae5f1900b94
Leo Chen <a0952250818@gmail.com>
2018-10-31 23:05:05 +0800

    新增列舉

---
+++ src/app/todo-list/todo-status-type.enum.ts
@@ -0,0 +1,24 @@
+/**
+ * 待辦事項的狀態類型列舉
+ *
+ * @export
+ * @enum {number}
+ */
+export enum TodoStatusType {
+
+  /**
+   * 所有
+   */
+  All,
+
+  /**
+   * 正在進行
+   */
+  Active,
+
+  /**
+   * 已完成
+   */
+  Completed
+
+}
